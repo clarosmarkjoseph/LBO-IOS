97b89d428dd235dde48828a2b17963f334b9ed5e
Paolo Hilario <paolohilario@paolos-macbook-pro.local>
2018-05-21 11:35:11 +0800

    Sample commit

---
+++ SampleProject/MainMenuController.swift
@@ -0,0 +1,25 @@
+//
+//  MainMenuController.swift
+//  SampleProject
+//
+//  Created by Paolo Hilario on 5/21/18.
+//  Copyright © 2018 itadmin. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
